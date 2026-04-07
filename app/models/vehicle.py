9c7c6aa07800400fbdd34e37f53e133d42e2a34b
Martin <cermak_martin@centrum.cz>
2026-04-07 20:30:05 +0200

    Revert "Dev/models"

--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import relationship
 from .base import Base
 
 
@@ -11,11 +10,7 @@ class Vehicle(Base):
     model = Column(String(50), nullable=False)
     license_plate = Column(String(10))
     color = Column(String(30))
-    color_hex = Column(String(7), default="#FFFFFF")
     is_active = Column(Boolean, default=True)
-
-    # Vazba na záznamy o tankování
-    fuel_records = relationship('FuelRecord', back_populates='vehicle', cascade='all, delete')
 
     def __repr__(self):
         return f"{self.id}: {self.brand} {self.model} ({self.license_plate})"
--- app/services/vehicle_service.py
+++ app/services/vehicle_service.py
@@ -7,11 +7,6 @@ class VehicleService:
 
     def get_all_vehicles(self):
         return self.session.query(Vehicle).all()
-    
-    def get_vehicle(self, vehicle_id):
-        return (self.session.query(Vehicle)
-        .filter(Vehicle.id == vehicle_id)
-        .first())
 
     def create_vehicle(self, brand, model, license_plate=None, color=None):
         if not brand or not model:
@@ -29,32 +24,14 @@ class VehicleService:
 
         return vehicle
     
-    def delete_vehicle(self, vehicle_id):
-        vehicle = self.session.query(Vehicle).filter(Vehicle.id == vehicle_id).first()
-        if not vehicle:
-            raise ValueError("Vozidlo s daným ID nebylo nalezeno.")
-        self.session.delete(vehicle)
-        self.session.commit()
+    def delete_vehicle():
+        pass
 
-    def edit_vehicle(self, vehicle_id, brand=None, model=None, license_plate=None, color=None):
-        vehicle = self.get_vehicle(vehicle_id)
-        if not vehicle:
-            raise ValueError("Vozidlo s daným ID nebylo nalezeno.")
+    def edit_vehicle():
+        pass
 
-        if brand is not None:
-            vehicle.brand = brand
-        if model is not None:
-            vehicle.model = model
-        if license_plate is not None:
-            vehicle.license_plate = license_plate
-        if color is not None:
-            vehicle.color = color
-
-        self.session.commit()
-        return vehicle
-
-    def activate_vehicle(self, vehicle_id):
-        self.edit_vehicle(vehicle_id, is_active=True)
+    def activate_vehicle():
+        pass
 
     def deactivate_vehicle():
         pass
